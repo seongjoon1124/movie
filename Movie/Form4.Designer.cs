6912f1ee4855b384c9eebe3d43b0fae8546f5e4d
SeongJoon Park <hongpack9811@naver.com>
2022-06-16 01:45:32 +0900

    a

--- Movie/Form4.Designer.cs
+++ Movie/Form4.Designer.cs
@@ -30,13 +30,16 @@
         {
             this.textBox1 = new System.Windows.Forms.TextBox();
             this.button1 = new System.Windows.Forms.Button();
-            this.textBox2 = new System.Windows.Forms.TextBox();
             this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.comboBox2 = new System.Windows.Forms.ComboBox();
+            this.button2 = new System.Windows.Forms.Button();
+            this.dateTimePicker1 = new System.Windows.Forms.DateTimePicker();
+            this.button3 = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
             // textBox1
             // 
-            this.textBox1.Location = new System.Drawing.Point(197, 28);
+            this.textBox1.Location = new System.Drawing.Point(118, 12);
             this.textBox1.Multiline = true;
             this.textBox1.Name = "textBox1";
             this.textBox1.Size = new System.Drawing.Size(326, 43);
@@ -53,31 +56,63 @@
             this.button1.UseVisualStyleBackColor = true;
             this.button1.Click += new System.EventHandler(this.button1_Click);
             // 
-            // textBox2
-            // 
-            this.textBox2.Location = new System.Drawing.Point(197, 120);
-            this.textBox2.Multiline = true;
-            this.textBox2.Name = "textBox2";
-            this.textBox2.Size = new System.Drawing.Size(312, 76);
-            this.textBox2.TabIndex = 2;
-            this.textBox2.TextChanged += new System.EventHandler(this.textBox2_TextChanged);
-            // 
             // comboBox1
             // 
             this.comboBox1.FormattingEnabled = true;
-            this.comboBox1.Location = new System.Drawing.Point(197, 212);
+            this.comboBox1.Location = new System.Drawing.Point(118, 61);
             this.comboBox1.Name = "comboBox1";
-            this.comboBox1.Size = new System.Drawing.Size(272, 20);
+            this.comboBox1.Size = new System.Drawing.Size(117, 20);
             this.comboBox1.TabIndex = 3;
             this.comboBox1.SelectedIndexChanged += new System.EventHandler(this.comboBox1_SelectedIndexChanged);
             // 
+            // comboBox2
+            // 
+            this.comboBox2.FormattingEnabled = true;
+            this.comboBox2.Location = new System.Drawing.Point(302, 61);
+            this.comboBox2.Name = "comboBox2";
+            this.comboBox2.Size = new System.Drawing.Size(121, 20);
+            this.comboBox2.TabIndex = 4;
+            this.comboBox2.SelectedIndexChanged += new System.EventHandler(this.comboBox2_SelectedIndexChanged);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(241, 61);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(55, 23);
+            this.button2.TabIndex = 5;
+            this.button2.Text = "선택";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // dateTimePicker1
+            // 
+            this.dateTimePicker1.Location = new System.Drawing.Point(429, 61);
+            this.dateTimePicker1.Name = "dateTimePicker1";
+            this.dateTimePicker1.Size = new System.Drawing.Size(200, 21);
+            this.dateTimePicker1.TabIndex = 6;
+            this.dateTimePicker1.ValueChanged += new System.EventHandler(this.dateTimePicker1_ValueChanged);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(635, 62);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(55, 23);
+            this.button3.TabIndex = 7;
+            this.button3.Text = "선택";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
             // Form4
             // 
+            this.AllowDrop = true;
             this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 12F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.dateTimePicker1);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.comboBox2);
             this.Controls.Add(this.comboBox1);
-            this.Controls.Add(this.textBox2);
             this.Controls.Add(this.button1);
             this.Controls.Add(this.textBox1);
             this.Name = "Form4";
@@ -91,7 +126,10 @@
 
         private System.Windows.Forms.TextBox textBox1;
         private System.Windows.Forms.Button button1;
-        private System.Windows.Forms.TextBox textBox2;
         private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.ComboBox comboBox2;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.DateTimePicker dateTimePicker1;
+        private System.Windows.Forms.Button button3;
     }
 }
--- Movie/Form4.cs
+++ Movie/Form4.cs
@@ -25,6 +25,8 @@ namespace Movie
         {
             InitializeComponent();
             GetMovie_Area_time();
+            initRoot();
+          
         }
 
 
@@ -32,25 +34,25 @@ namespace Movie
         {
             Form2 _form = new Form2();
             String url = _form.GetTicketUrl();
-
+        
             textBox1.Text += "주소 : " + url;
             //String test = "https://www.naver.com";
 
             //IWebDriver driver = new ChromeDriver("C:/Users/Sungjun/Desktop/ExamSelenium/packages/Selenium.WebDriver.ChromeDriver.102.0.5005.6102/driver/win32");
             //IWebDriver a = new ChromeDriver();
-            //System.Diagnostics.Process.Start(url);
+            
 
 
-            using (IWebDriver Driver = new ChromeDriver("C:/Users/Sungjun/Desktop/ExamSelenium/packages/Selenium.WebDriver.ChromeDriver.102.0.5005.6102/driver/win32")) //크롬 드라이버 안맞아서 경로 지정
+            using (IWebDriver Driver = new ChromeDriver("C:/Users/Sungjun/AndroidStudioProjects/movie/app")) //크롬 드라이버 안맞아서 경로 지정
             {
 
-
+                System.Diagnostics.Process.Start(url);
                 Driver.Url = url;
+                //var element = Driver.FindElement(By.Id("rootDropBox"));
+                //comboBox1.Items.Add(element.Text);
 
-                var element = Driver.FindElement(By.XPath("//*[@id='rootDropBox']"));
-                comboBox1.Items.Add(element.Text);
-
-                IList<IWebElement> elements;
+                //IList<IWebElement> elements;
+                
                 //foreach (var item in elements)
                 //{
                 //    comboBox1.Items.Add((item).Text);
@@ -66,10 +68,214 @@ namespace Movie
 
         }
 
+        public void initRoot()
+        {
+            comboBox1.DisplayMember = "value";
+            comboBox1.ValueMember = "key";
+            Dictionary<string, string> rootArea = new Dictionary<string, string>();
+            rootArea.Add("1", "서울");
+            rootArea.Add("2", "인천");
+            rootArea.Add("3", "경기");
+            rootArea.Add("4", "대전");
+            rootArea.Add("5", "충청");
+            rootArea.Add("6", "강원");
+            rootArea.Add("7", "광주");
+            rootArea.Add("8", "전라");
+            rootArea.Add("9", "부산");
+            rootArea.Add("10", "대구");
+            rootArea.Add("11", "경상");
+            rootArea.Add("12", "제주");
+
+            comboBox1.DataSource = new BindingSource(rootArea, null);   
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            initSub();
+        }
+        public void initSub()
+        {
+            string sub_value = comboBox1.SelectedValue.ToString(); 
+            if(sub_value == "1")
+            {
+                comboBox2.DisplayMember = "value";
+                comboBox2.ValueMember = "key";
+                Dictionary<string, string> subArea = new Dictionary<string, string>();
+                subArea.Add("53", "강남구/서초구");
+                subArea.Add("54", "강동구/송파구/광진구");
+                subArea.Add("55", "강북구/도봉구/노원구");
+                subArea.Add("56", "강서구/양천구");
+                subArea.Add("57", "관악구/동작구");
+                subArea.Add("58", "구로구/금천구/영등포구");
+                subArea.Add("59", "마포구/서대문구");
+                subArea.Add("60", "동대문구/성동구");
+                subArea.Add("61", "성북구/중랑구");
+                subArea.Add("62", "은평구");
+                subArea.Add("63", "종로구");
+                subArea.Add("64", "중구");
+                subArea.Add("65", "용산구");
+
+                comboBox2.DataSource = new BindingSource(subArea, null);
+            }
+
+            
+            if (sub_value == "2")
+            {
+                comboBox2.DisplayMember = "value";
+                comboBox2.ValueMember = "key";
+                Dictionary<string, string> subArea = new Dictionary<string, string>();
+                subArea.Add("46", "인천");
+            
+
+                comboBox2.DataSource = new BindingSource(subArea, null);
+            }
+
+            if (sub_value == "3")
+            {
+                comboBox2.DisplayMember = "value";
+                comboBox2.ValueMember = "key";
+                Dictionary<string, string> subArea = new Dictionary<string, string>();
+                subArea.Add("66", "수원/화성");
+                subArea.Add("67", "시흥/안산");
+                subArea.Add("68", "안양/군포/의왕");
+                subArea.Add("69", "광명/부천");
+                subArea.Add("70", "고양/파주");
+                subArea.Add("71", "성남/용인/하남");
+                subArea.Add("72", "오산/안성/평택");
+                subArea.Add("73", "남양주/구리");
+                subArea.Add("74", "여주/이천");
+                subArea.Add("75", "의정부");
+                subArea.Add("76", "김포");
+                subArea.Add("77", "동두천/양주");
+                subArea.Add("78", "광주");
+                subArea.Add("83", "포천");
+                subArea.Add("85", "가평");
+
+                comboBox2.DataSource = new BindingSource(subArea, null);
+            }
+
+            if (sub_value == "4")
+            {
+                comboBox2.DisplayMember = "value";
+                comboBox2.ValueMember = "key";
+                Dictionary<string, string> subArea = new Dictionary<string, string>();
+                subArea.Add("47", "대전");
+
+                comboBox2.DataSource = new BindingSource(subArea, null);
+            }
+
+            if (sub_value == "5")
+            {
+                comboBox2.DisplayMember = "value";
+                comboBox2.ValueMember = "key";
+                Dictionary<string, string> subArea = new Dictionary<string, string>();
+                subArea.Add("48", "충청");
+
+                comboBox2.DataSource = new BindingSource(subArea, null);
+            }
+
+            if (sub_value == "6")
+            {
+                comboBox2.DisplayMember = "value";
+                comboBox2.ValueMember = "key";
+                Dictionary<string, string> subArea = new Dictionary<string, string>();
+                subArea.Add("49", "강원");
+
+                comboBox2.DataSource = new BindingSource(subArea, null);
+            }
+
+            if (sub_value == "7")
+            {
+                comboBox2.DisplayMember = "value";
+                comboBox2.ValueMember = "key";
+                Dictionary<string, string> subArea = new Dictionary<string, string>();
+                subArea.Add("50", "광주");
+
+                comboBox2.DataSource = new BindingSource(subArea, null);
+            }
+
+            if (sub_value == "8")
+            {
+                comboBox2.DisplayMember = "value";
+                comboBox2.ValueMember = "key";
+                Dictionary<string, string> subArea = new Dictionary<string, string>();
+                subArea.Add("24", "전라");
+
+                comboBox2.DataSource = new BindingSource(subArea, null);
+            }
+
+            if (sub_value == "9")
+            {
+                comboBox2.DisplayMember = "value";
+                comboBox2.ValueMember = "key";
+                Dictionary<string, string> subArea = new Dictionary<string, string>();
+                subArea.Add("26", "금정구");
+                subArea.Add("27", "남구");
+                subArea.Add("28", "동래구");
+                subArea.Add("29", "부산진구");
+                subArea.Add("30", "북구");
+                subArea.Add("31", "사상구");
+                subArea.Add("32", "연제구");
+                subArea.Add("33", "중구");
+                subArea.Add("34", "해운대구");
+                subArea.Add("79", "기장군");
+                subArea.Add("80", "사하구");
+                subArea.Add("81", "강서구");
+
+                comboBox2.DataSource = new BindingSource(subArea, null);
+            }
+
+            if (sub_value == "10")
+            {
+                comboBox2.DisplayMember = "value";
+                comboBox2.ValueMember = "key";
+                Dictionary<string, string> subArea = new Dictionary<string, string>();
+                subArea.Add("51", "대구");
+
+                comboBox2.DataSource = new BindingSource(subArea, null);
+            }
+
+            if (sub_value == "11")
+            {
+                comboBox2.DisplayMember = "value";
+                comboBox2.ValueMember = "key";
+                Dictionary<string, string> subArea = new Dictionary<string, string>();
+                subArea.Add("35", "거제/통영");
+                subArea.Add("36", "거창");
+                subArea.Add("37", "김해/양산");
+                subArea.Add("38", "창원/마산");
+                subArea.Add("39", "밀양");
+                subArea.Add("40", "진주");
+                subArea.Add("41", "경산");
+                subArea.Add("42", "경주/포항");
+                subArea.Add("43", "구미/김천");
+                subArea.Add("44", "안동");
+                subArea.Add("45", "울산");
+                subArea.Add("84", "의성");
+                subArea.Add("86", "산청");
+
+                comboBox2.DataSource = new BindingSource(subArea, null);
+            }
+
+            if (sub_value == "12")
+            {
+                comboBox2.DisplayMember = "value";
+                comboBox2.ValueMember = "key";
+                Dictionary<string, string> subArea = new Dictionary<string, string>();
+                subArea.Add("52", "제주");
+
+                comboBox2.DataSource = new BindingSource(subArea, null);
+            }
+
+
+        }
 
 
 
-            private void textBox1_TextChanged(object sender, EventArgs e)
+
+
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
         {
 
         }
@@ -82,6 +288,18 @@ namespace Movie
             Program.ac.MainForm = newForm;
             this.Close();
         }
+        private void button3_Click(object sender, EventArgs e)
+        {
+            string date = dateTimePicker1.Value.ToString("yyyy-MM-dd");
+            MessageBox.Show(date);
+
+        }
+
+
+
+
+
+
 
         private void textBox2_TextChanged(object sender, EventArgs e)
         {
@@ -92,5 +310,17 @@ namespace Movie
         {
 
         }
+
+        private void comboBox2_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void dateTimePicker1_ValueChanged(object sender, EventArgs e)
+        {
+
+        }
+
+      
     }
 }
